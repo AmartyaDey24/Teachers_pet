7e77dbf691055cee48d77ea6056880bf46391d52
AMARTYA <amartyadey24@gmail.com>
2021-03-16 21:25:36 +0530

    Teachers Pet

--- app/src/main/java/com/example/teacherspet/MainActivity.java
+++ app/src/main/java/com/example/teacherspet/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.teacherspet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         Button button1;
+         button1 = (Button)findViewById(R.id.button1);
+         button1.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 student_login();
+             }
+         });
+    }
+    public void  student_login()
+    {
+        Intent intent = new Intent(this, student_login.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/teacherspet/student_login.java
@@ -0,0 +1,15 @@
+package com.example.teacherspet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+        setTitle("Student Login");
+    }
+}
